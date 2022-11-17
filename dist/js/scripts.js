d0ebac8c6b0aab56529063baf85f63ff2bf70de2
unknown <reeallysong@gmail.com>
2022-11-17 21:57:01 +0100

    Header y menu movil terminados

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,12 +1,25 @@
-const menu = document.getElementById('menu')
-const hamburguer = document.getElementById('hamburguer')
+const menuElement = document.getElementById('menu')
+const hamburguerElement = document.getElementById('hamburguer')
 const closeElement = document.getElementById('close')
+const lineElement = document.getElementById('line')
+const bodyElement = document.getElementById('body')
+const cursor = document.getElementById("cursor");
+const headerElement = document.getElementById("header");
+const rootStyles = document.documentElement.style;
 
-hamburguer.addEventListener('click', () => {
+
+hamburguerElement.addEventListener('click', () => {
     menu.classList.toggle('menu--show')
-    
+    lineElement.classList.toggle('line--cross');
+    bodyElement.classList.toggle('body--menu')
 });
 
-closeElement.addEventListener('click', () => {
-    menu.classList.toggle('menu--show')
-})
+window.addEventListener("mousemove", (ev) => {
+  rootStyles.setProperty("--x-cord", ev.x + "px");
+  rootStyles.setProperty("--y-cord", ev.y + "px");
+});
+
+headerElement.addEventListener("mouseenter", () => {
+  rootStyles.setProperty("--cursor-color", "#EAE1F4");
+  console.log("pink");
+});
